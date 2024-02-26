525653226ec8d0d30954815b7d4553cb94a59915
PhanHung <phanquochung280702@gmail.com>
2024-02-26 12:02:42 +0700

    update cv

--- src/components/header/component/CTA.jsx
+++ src/components/header/component/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function CTA() {
   return (
     <div className="cta">
-        <a className='btn' href="./pdf/Fullstack_Intern_PhanQuocHung.pdf">Download CV</a>
+        <a className='btn' href="./pdf/Intern_FrontEnd_PhanQuocHung.pdf">Download CV</a>
         <a className='btn btn-primary' href="#contact">Lest's Talk</a>
     </div>
   )
